de78c19bae4db8f1d46a4b48306dc9da7b4ebfe0
mengkongly <mengkong2009@yahoo.com>
2018-11-02 16:43:42 -0500

    Insert address to db by route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\User;
+use App\Address;
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -13,4 +15,20 @@
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/user/{id}/in_address/',function($id){
+    $user       =   User::findOrFail($id);
+
+    //$address    =   new Address(['name'=>'4464 Sapphire Dr, Frisco, TX']);
+    $address    =   new Address(['name'=>'44 Legacy, Frisco, TX']);
+
+
+    // address() is call function in User model
+    if($user->address()->save($address)){
+        return 'Insert successfully';
+    }
+    return 'Insert failed';
+
 });
